f47cb97342cfd199883743b0d7a74adfdb9ac481
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-02-04 14:12:12 +0000

    Update: Header sort.h file

--- sort.h
+++ sort.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 /* Print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
 /* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
