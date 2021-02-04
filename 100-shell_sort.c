1c6d16f93e871c702aeebb41d955f47bfa407754
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-02-04 15:40:56 +0000

    Created: Shell sort algorithm.

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * shell_sort - Shell sot algorithm.
+ * @array: an array to sort.
+ * @size: size of the array.
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t i, gap = 0, check;
+	int tmp;
+
+	while (gap < size)
+		gap = gap * 3 + 1;
+	gap = (gap - 1) / 3;
+
+	while (gap > 0)
+	{
+		check = 0;
+		for (i = 0; i + gap < size; i++)
+		{
+			if (array[i] > array[i + gap])
+			{
+				tmp = array[i];
+				array[i] = array[i + gap];
+				array[i + gap] = tmp;
+				check = 1;
+			}
+		}
+		if (check == 0)
+		{
+			gap = (gap - 1) / 3;
+			print_array(array, size);
+		}
+	}
+}
